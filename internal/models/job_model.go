0dbdb68b30135f84c7243a02cea32120df552aae
Nemanja Vujic <nvujic2002@gmail.com>
2024-10-03 10:24:58 +0200

    added Json tags to model for API usages

--- internal/models/job_model.go
+++ internal/models/job_model.go
@@ -1,13 +1,14 @@
 package models
 
 // Job represents a job listing
-// TODO: Figure out how to generate keywords for each job (myb gpt1.0)
+// TODO: Figure out how to generate keywords for each job (myb gpt1.0) hugging face api is good too
+
 type Job struct {
-	ID        int
-	Title     string
-	Company   string
-	Location  string
-	Seniority string
-	URL       string
-	Site      string
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Company   string `json:"company"`
+	Location  string `json:"location"`
+	Seniority string `json:"seniority"`
+	URL       string `json:"url"`
+	Site      string `json:"site"`
 }
